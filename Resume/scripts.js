6b132d402b7f6b84918796bb7d9e552abfb207e4
christycaldwell77 <59845898+christycaldwell77@users.noreply.github.com>
2020-03-03 13:49:09 -0700

    Add files via upload

---
+++ Resume/scripts.js
@@ -0,0 +1,16 @@
+var accordions =
+document.getElementsByClassName("accordion")
+
+for (var i =0; i <accordions.length; i++) {
+  accordions[i].onclick = function (){
+    var content = this.nextElementSibling;
+
+    if (content.style.maxHeight) {
+      //accordions's open
+      content.style.maxHeight = null;
+    } else {
+      //accordions's closed
+      content.style.maxHeight = content.scrollHeight + "px";
+    }
+  }
+}
